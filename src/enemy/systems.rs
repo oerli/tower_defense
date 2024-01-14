5af9d4e773c7a1fe55b0e8dbca64e6480d774b69
Roland Mueller <1357906+oerli@users.noreply.github.com>
2024-01-14 17:01:40 +0100

    change animation for each enemy when dead

--- src/enemy/systems.rs
+++ src/enemy/systems.rs
@@ -1,10 +1,12 @@
 use std::f32::consts::PI;
+use std::time::Duration;
 
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
 
 use crate::level::components::*;
 use crate::player::resources::*;
+use crate::resources::*;
 
 use super::components::*;
 
@@ -24,7 +26,7 @@ pub fn enemy_movement(
     let level = query_level.get_single().unwrap();
 
     for (entity, mut enemy, mut velocity, position, mut transform) in query.iter_mut() {
-        if enemy.waypoint < level.waypoints.len() {
+        if enemy.waypoint < level.waypoints.len() && enemy.health > 0 {
             transform.look_at(level.waypoints[enemy.waypoint], Vec3::ZERO);
             // TODO: dirty hack to rotate the enemy 180 degrees
             transform.rotate(Quat::from_rotation_y(PI));
@@ -39,7 +41,7 @@ pub fn enemy_movement(
                 direction = direction.normalize();
                 velocity.linvel += direction * enemy.speed;
             }
-        } else {
+        } else if enemy.health > 0 {
             // enemy reached goal
             player.lives -= 1;
             commands.entity(entity).despawn();
@@ -49,14 +51,25 @@ pub fn enemy_movement(
 
 pub fn enemy_destroyed(
     mut commands: Commands,
-    mut query: Query<(Entity, &Enemy)>,
+    query: Query<(Entity, &Enemy)>,
     mut player: ResMut<Player>,
-
+    mut animation_players: Query<&mut AnimationPlayer>,
+    animations: Res<Animations>,
+    children: Query<&Children>
 ) {
-    for (entity, enemy) in query.iter_mut() {
+    for (entity, enemy) in query.iter() {
         if enemy.health <= 0 {
             player.score += enemy.score;
-            commands.entity(entity).despawn();
+
+            // find an animation player among the descendants of the thing, there's only one in my case
+            for entity in children.iter_descendants(entity) {
+                if let Ok(mut animation_player) = animation_players.get_mut(entity) {
+                    animation_player.play_with_transition(animations.0[1].clone_weak(), Duration::from_millis(250));
+                }
+            }
+
+            // TODO: add a delay before despawning the enemy
+            commands.entity(entity).remove::<Enemy>();
         }
     }
 }
