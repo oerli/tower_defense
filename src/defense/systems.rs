1ec4f291f7c4e7f448f2c4238d4bef423fe3679c
Roland Mueller <1357906+oerli@users.noreply.github.com>
2024-01-14 10:32:16 +0100

    fixed enemy shooting at new target when first target left

--- src/defense/systems.rs
+++ src/defense/systems.rs
@@ -5,7 +5,7 @@ use super::components::*;
 
 use crate::{bullet::components::*, components::*, enemy::components::*};
 
-// TODO: some targets might miss, if there is an enemy who has been destoryed
+// TODO: might be a better way to do this
 pub fn defense_shooting(
     mut commands: Commands,
     mut query: Query<(&mut Defense, &GlobalTransform)>,
@@ -22,34 +22,40 @@ pub fn defense_shooting(
         }
 
         // check if target is still alive
-        if let Some(target) = defense.targets.pop() {
-            if let Ok(enemy) = enemy_query.get(target) {
-                commands.spawn((
-                    PbrBundle {
-                        mesh: meshes.add(Mesh::try_from(shape::Box::new(0.2, 0.2, 0.2)).unwrap()),
-                        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-                        transform: Transform::from_translation(
-                            transform.translation() + Vec3::new(0.0, 0.5, 0.0),
-                        ),
-                        ..default()
-                    },
-                    RigidBody::Dynamic,
-                    Collider::cuboid(0.1, 0.1, 0.1),
-                    ExternalImpulse {
-                        impulse: (enemy.translation() - transform.translation()) * 0.05,
-                        torque_impulse: Vec3::new(0.0, 0.0, 0.0),
-                    },
-                    Bullet::new(enemy.translation(), 1.0, defense.damage),
-                    CollisionGroups::new(Group::GROUP_1, Group::GROUP_3 | Group::GROUP_4),
-                    Lifetime {
-                        timer: Timer::from_seconds(1.0, TimerMode::Once),
-                    },
-                ));
-
-                defense.shooting_timer.reset();
-                // put back the target
-                defense.targets.insert(0, target);
+        loop {
+            if let Some(target) = defense.targets.pop() {
+                if let Ok(enemy) = enemy_query.get(target) {
+                    commands.spawn((
+                        PbrBundle {
+                            mesh: meshes.add(Mesh::try_from(shape::Box::new(0.2, 0.2, 0.2)).unwrap()),
+                            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+                            transform: Transform::from_translation(
+                                transform.translation() + Vec3::new(0.0, 0.5, 0.0),
+                            ),
+                            ..default()
+                        },
+                        RigidBody::Dynamic,
+                        Collider::cuboid(0.1, 0.1, 0.1),
+                        ExternalImpulse {
+                            impulse: (enemy.translation() - transform.translation()) * 0.05,
+                            torque_impulse: Vec3::new(0.0, 0.0, 0.0),
+                        },
+                        Bullet::new(enemy.translation(), 1.0, defense.damage),
+                        CollisionGroups::new(Group::GROUP_1, Group::GROUP_3 | Group::GROUP_4),
+                        Lifetime {
+                            timer: Timer::from_seconds(1.0, TimerMode::Once),
+                        },
+                    ));
+    
+                    defense.shooting_timer.reset();
+                    // put back the target
+                    defense.targets.insert(0, target);
+                    break;
+                }
+            } else {
+                break;
             }
         }
+        
     }
 }
