322f69cb278d5a96886d3d15e5457068cc69e1d5
Roland Mueller <1357906+oerli@users.noreply.github.com>
2024-01-28 17:53:59 +0100

    centered menu and pause menu always shown

--- src/menu/layouts.rs
+++ src/menu/layouts.rs
@@ -1,10 +1,11 @@
 use bevy::prelude::*;
-use bevy_egui::{egui, EguiContexts, egui::Vec2};
+use bevy_egui::egui::{Align2, Stroke};
+use bevy_egui::{egui, egui::Vec2, EguiContexts};
 
-use crate::player::resources::*;
-use crate::GameState;
 use crate::defense::components::*;
 use crate::defense::resources::*;
+use crate::player::resources::*;
+use crate::GameState;
 use crate::HoverHandler;
 
 pub fn tower_selection(
@@ -23,7 +24,7 @@ pub fn tower_selection(
         .resizable(false)
         .collapsible(false)
         .title_bar(false)
-        .default_pos([140.0, 140.0])
+        .anchor(Align2::CENTER_CENTER, Vec2::new(0.0, 0.0))
         .show(contexts.ctx_mut(), |ui| {
             egui::Grid::new("selection").striped(true).show(ui, |ui| {
                 if ui
@@ -37,29 +38,30 @@ pub fn tower_selection(
 
                     // create cannon tower for hover events
                     if hover_handler.entity.is_some() {
-                        commands.entity(hover_handler.entity.unwrap()).despawn_recursive();
+                        commands
+                            .entity(hover_handler.entity.unwrap())
+                            .despawn_recursive();
                     }
                     hover_handler.entity = Some(
                         commands
                             .spawn(SceneBundle {
                                 scene: asset_server.load("models/cannon_tower.glb#Scene0"),
                                 ..Default::default()
-                            }).with_children(|parent|{
-                                parent.spawn((
-                                    PbrBundle {
-                                        mesh: asset_server.add(Mesh::from(shape::Torus {
-                                            radius: 2.96,
-                                            ring_radius: 0.02,
-                                            ..Default::default()
-                                        })),
-                                        material: asset_server.add(StandardMaterial {
-                                            base_color: Color::rgb(0.8, 0.2, 0.2),
-                                            ..Default::default()
-                                        }),
-                                        transform: Transform::from_xyz(0.0, 0.3, 0.0),
+                            })
+                            .with_children(|parent| {
+                                parent.spawn((PbrBundle {
+                                    mesh: asset_server.add(Mesh::from(shape::Torus {
+                                        radius: 2.96,
+                                        ring_radius: 0.02,
                                         ..Default::default()
-                                    },
-                                ));
+                                    })),
+                                    material: asset_server.add(StandardMaterial {
+                                        base_color: Color::rgb(0.8, 0.2, 0.2),
+                                        ..Default::default()
+                                    }),
+                                    transform: Transform::from_xyz(0.0, 0.3, 0.0),
+                                    ..Default::default()
+                                },));
                             })
                             .id(),
                     );
@@ -78,29 +80,30 @@ pub fn tower_selection(
 
                     // create ballista tower for hover events
                     if hover_handler.entity.is_some() {
-                        commands.entity(hover_handler.entity.unwrap()).despawn_recursive();
+                        commands
+                            .entity(hover_handler.entity.unwrap())
+                            .despawn_recursive();
                     }
                     hover_handler.entity = Some(
                         commands
                             .spawn(SceneBundle {
                                 scene: asset_server.load("models/ballista_tower.glb#Scene0"),
                                 ..Default::default()
-                            }).with_children(|parent|{
-                                parent.spawn((
-                                    PbrBundle {
-                                        mesh: asset_server.add(Mesh::from(shape::Torus {
-                                            radius: 2.96,
-                                            ring_radius: 0.02,
-                                            ..Default::default()
-                                        })),
-                                        material: asset_server.add(StandardMaterial {
-                                            base_color: Color::rgb(0.8, 0.2, 0.2),
-                                            ..Default::default()
-                                        }),
-                                        transform: Transform::from_xyz(0.0, 0.3, 0.0),
+                            })
+                            .with_children(|parent| {
+                                parent.spawn((PbrBundle {
+                                    mesh: asset_server.add(Mesh::from(shape::Torus {
+                                        radius: 2.96,
+                                        ring_radius: 0.02,
                                         ..Default::default()
-                                    },
-                                ));
+                                    })),
+                                    material: asset_server.add(StandardMaterial {
+                                        base_color: Color::rgb(0.8, 0.2, 0.2),
+                                        ..Default::default()
+                                    }),
+                                    transform: Transform::from_xyz(0.0, 0.3, 0.0),
+                                    ..Default::default()
+                                },));
                             })
                             .id(),
                     );
@@ -119,7 +122,9 @@ pub fn tower_selection(
 
                     // create archer tower for hover events
                     if hover_handler.entity.is_some() {
-                        commands.entity(hover_handler.entity.unwrap()).despawn_recursive();
+                        commands
+                            .entity(hover_handler.entity.unwrap())
+                            .despawn_recursive();
                     }
                     hover_handler.entity = Some(
                         commands
@@ -127,22 +132,20 @@ pub fn tower_selection(
                                 scene: asset_server.load("models/archer_tower.glb#Scene0"),
                                 ..Default::default()
                             })
-                            .with_children(|parent|{
-                                parent.spawn((
-                                    PbrBundle {
-                                        mesh: asset_server.add(Mesh::from(shape::Torus {
-                                            radius: 1.96,
-                                            ring_radius: 0.02,
-                                            ..Default::default()
-                                        })),
-                                        material: asset_server.add(StandardMaterial {
-                                            base_color: Color::rgb(0.8, 0.2, 0.2),
-                                            ..Default::default()
-                                        }),
-                                        transform: Transform::from_xyz(0.0, 0.3, 0.0),
+                            .with_children(|parent| {
+                                parent.spawn((PbrBundle {
+                                    mesh: asset_server.add(Mesh::from(shape::Torus {
+                                        radius: 1.96,
+                                        ring_radius: 0.02,
                                         ..Default::default()
-                                    },
-                                ));
+                                    })),
+                                    material: asset_server.add(StandardMaterial {
+                                        base_color: Color::rgb(0.8, 0.2, 0.2),
+                                        ..Default::default()
+                                    }),
+                                    transform: Transform::from_xyz(0.0, 0.3, 0.0),
+                                    ..Default::default()
+                                },));
                             })
                             .id(),
                     );
@@ -161,22 +164,21 @@ pub fn tower_selection(
         });
 }
 
-
-pub fn high_scores(
-    mut contexts: EguiContexts,
-    player: Res<Player>,
-) {
+pub fn high_scores(mut contexts: EguiContexts, player: Res<Player>) {
     egui::Window::new("High Scores")
         .resizable(false)
         .collapsible(false)
         .title_bar(false)
-        .default_pos([140.0, 140.0])
+        .anchor(Align2::CENTER_CENTER, Vec2::new(0.0, 0.0))
         .show(contexts.ctx_mut(), |ui| {
             egui::Grid::new("players").striped(true).show(ui, |ui| {
                 ui.label("Game Over!");
                 ui.end_row();
 
-                ui.label(format!("Player\nName: {}\tLevel: {}\tLives: {}\tScore: {}\tCredits:{}", player.name, player.level, player.lives, player.score, player.credits));
+                ui.label(format!(
+                    "Player\nName: {}\tLevel: {}\tLives: {}\tScore: {}\tCredits:{}",
+                    player.name, player.level, player.lives, player.score, player.credits
+                ));
                 ui.end_row();
             });
         });
@@ -185,20 +187,51 @@ pub fn high_scores(
 pub fn show_pause(
     mut contexts: EguiContexts,
     mut next_game_state: ResMut<NextState<GameState>>,
+    asset_server: Res<AssetServer>,
+    game_state: Res<State<GameState>>,
 ) {
+    let play_image = contexts.add_image(asset_server.load("images/play.png"));
+    let pause_image = contexts.add_image(asset_server.load("images/pause.png"));
+
+    let (play_enabled, pause_enabled) = if *game_state.get() == GameState::Paused {
+        (true, false)
+    } else if *game_state.get() == GameState::Playing {
+        (false, true)
+    } else {
+        (false, false)
+    };
+
     egui::Window::new("Pause Menu")
         .resizable(false)
         .collapsible(false)
         .title_bar(false)
-        .default_pos([140.0, 140.0])
+        .anchor(Align2::CENTER_TOP, Vec2::new(0.0, 0.0))
         .show(contexts.ctx_mut(), |ui| {
             egui::Grid::new("pause").striped(true).show(ui, |ui| {
                 if ui
-                    .add(egui::Button::new("Resume").min_size(Vec2::new(80.0, 80.0)))
+                    .add_enabled(
+                        play_enabled,
+                        egui::Button::image(egui::load::SizedTexture::new(
+                            play_image,
+                            [48.0, 48.0],
+                        )),
+                    )
                     .clicked()
                 {
                     next_game_state.set(GameState::Playing);
-                }
+                };
+                if ui
+                    .add_enabled(
+                        pause_enabled,
+                        egui::Button::image(egui::load::SizedTexture::new(
+                            pause_image,
+                            [48.0, 48.0],
+                        )),
+                    )
+                    .clicked()
+                {
+                    next_game_state.set(GameState::Paused);
+                };
                 ui.end_row();
             });
         });
