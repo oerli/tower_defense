a57cfd6cc3cd18c03cfad59da62a603281f3f05d
Roland Mueller <1357906+oerli@users.noreply.github.com>
2024-01-30 08:05:05 +0100

    fixed archer shooting

--- src/defense/events.rs
+++ src/defense/events.rs
@@ -23,7 +23,6 @@ pub fn spawn_defense(
     query_defense_range: Query<Entity, With<DefenseRange>>,
 ) {
     for event in build_events.read() {
-
         // remove all current defense ranges (should only be one)
         for range_entity in query_defense_range.iter() {
             commands.entity(range_entity).despawn_recursive();
@@ -140,7 +139,8 @@ pub fn spawn_defense(
                                 shooting_timer: Timer::from_seconds(0.2, TimerMode::Repeating),
                                 targets: VecDeque::new(),
                             },
-                            Collider::ball(2.0),
+                            // with collidr 2.0 enemy will leave range instatnly
+                            Collider::ball(2.1),
                             Sensor,
                             CollisionGroups::new(Group::GROUP_2, Group::GROUP_3),
                         ))
@@ -152,7 +152,14 @@ pub fn spawn_defense(
                                 PickableBundle::default(),
                                 RapierPickable,
                             ));
-                            parent.spawn((Weapon::Archer,));
+                            parent.spawn((
+                                // weapon shoots and needs transform
+                                PbrBundle {
+                                    transform: Transform::from_xyz(0.0, 0.5, 0.0),
+                                    ..Default::default()
+                                },
+                                Weapon::Archer,
+                            ));
                         });
                 });
             }
@@ -219,25 +226,27 @@ pub fn range_event(
             // get collider range info from parent
             if let Ok(defense_collider) = collider_query.get(defense_parent.get()) {
                 // create defense range entity below defense entity
-                commands.entity(defense_parent.get()).with_children(|parent| {
-                    parent.spawn((
-                        PbrBundle {
-                            mesh: asset_server.add(Mesh::from(shape::Torus {
-                                // subtract ring_radius from range
-                                radius: defense_collider.as_ball().unwrap().radius() - 0.02,
-                                ring_radius: 0.02,
+                commands
+                    .entity(defense_parent.get())
+                    .with_children(|parent| {
+                        parent.spawn((
+                            PbrBundle {
+                                mesh: asset_server.add(Mesh::from(shape::Torus {
+                                    // subtract ring_radius from range
+                                    radius: defense_collider.as_ball().unwrap().radius() - 0.02,
+                                    ring_radius: 0.02,
+                                    ..Default::default()
+                                })),
+                                material: asset_server.add(StandardMaterial {
+                                    base_color: Color::rgb(0.8, 0.2, 0.2),
+                                    ..Default::default()
+                                }),
+                                transform: Transform::from_translation(Vec3::new(0.0, 0.3, 0.0)),
                                 ..Default::default()
-                            })),
-                            material: asset_server.add(StandardMaterial {
-                                base_color: Color::rgb(0.8, 0.2, 0.2),
-                                ..Default::default()
-                            }),
-                            transform: Transform::from_translation(Vec3::new(0.0, 0.3, 0.0)),
-                            ..Default::default()
-                        },
-                        DefenseRange,
-                    ));
-                });
+                            },
+                            DefenseRange,
+                        ));
+                    });
             }
         }
     }
@@ -252,10 +261,9 @@ pub fn enemy_contact(
         match collision_event {
             CollisionEvent::Started(entity1, entity2, _flags) => {
                 // add target to defense
-                if enemy_query.get(*entity1).is_ok() {  
+                if enemy_query.get(*entity1).is_ok() {
                     if let Ok(mut defense) = defense_query.get_mut(*entity2) {
                         defense.targets.push_back(*entity1);
-                        
                     }
                 } else if enemy_query.get(*entity2).is_ok() {
                     if let Ok(mut defense) = defense_query.get_mut(*entity1) {
@@ -266,7 +274,7 @@ pub fn enemy_contact(
             // does not work if enemy is despawned
             CollisionEvent::Stopped(entity1, entity2, _flags) => {
                 // remove target from defense
-                if enemy_query.get(*entity1).is_ok() {  
+                if enemy_query.get(*entity1).is_ok() {
                     if let Ok(mut defense) = defense_query.get_mut(*entity2) {
                         defense.targets.retain(|&x| x != *entity1);
                     }
@@ -274,7 +282,7 @@ pub fn enemy_contact(
                     if let Ok(mut defense) = defense_query.get_mut(*entity1) {
                         defense.targets.retain(|&x| x != *entity2);
                     }
-                } 
+                }
             }
         }
     }
